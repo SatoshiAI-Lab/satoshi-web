8c951971f16ec341553d45732d0bcc2daf40f5e7
baizhi958216 <1475289190@qq.com>
2024-04-07 14:25:23 +0800

    feat: exchange anno add message avatar
    * content switch to title
    * title switch to source name
    
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/components/chat/components/bubbles/exchange-announcement-bubble.tsx
+++ src/components/chat/components/bubbles/exchange-announcement-bubble.tsx
@@ -4,11 +4,13 @@ import clsx from 'clsx'
 import MessageBubble from './message-bubble'
 import { ChatResponseMetaAnnounceMent } from '@/api/chat/types'
 import dayjs from 'dayjs'
+import { Avatar } from '@mui/material'
 
 const ExchangeAnnouncementBubble = ({
   title,
   created_at,
   source_logo,
+  source_name,
   url,
   children,
 }: ChatResponseMetaAnnounceMent & { children: React.ReactNode }) => {
@@ -16,20 +18,23 @@ const ExchangeAnnouncementBubble = ({
     <MessageBubble className={clsx('min-w-bubble py-4')}>
       {/* Avatar, chain */}
       <div className="flex items-stretch">
-        <img
-          src="/images/i1.png"
-          alt="avatar"
-          className="w-12 h-12 rounded-full mr-2"
-        />
+        {(source_logo && (
+          <img
+            src={source_logo}
+            alt="avatar"
+            className="w-12 h-12 rounded-full mr-2"
+          />
+        )) || <Avatar className="w-12 h-12 rounded-full mr-2" />}
+
         <div className="flex flex-col justify-between ">
-          <span className="font-bold">{title.en}</span>
+          <span className="font-bold">{source_name}</span>
           <span className="text-gray-400">
             {dayjs(created_at).format('H:mm M/D')}
           </span>
         </div>
       </div>
       {/* Event description */}
-      <div className="mt-2 font-bold">{children}</div>
+      <div className="mt-2 font-bold">{title.en}</div>
       <a
         href={url.en}
         target="_blank"
