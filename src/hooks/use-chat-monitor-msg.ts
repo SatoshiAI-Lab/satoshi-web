0ea82200c7ffabceb95c6fc9c35d463fd15d172b
leo <1909802953@qq.com>
2024-04-10 19:04:05 +0800

    fix: Websocket prevents logout

--- src/hooks/use-chat-monitor-msg.ts
+++ src/hooks/use-chat-monitor-msg.ts
@@ -13,7 +13,7 @@ export const useChatMonitorMsg = () => {
   const { userInfo, isLogined } = useUserStore()
   const { setUnreadMessage } = useChatStore()
   const { addMonitorMessage } = useChat()
-  const { setSocket } = useChatStore()
+  const { socket, setSocket } = useChatStore()
   const baseURL = `${URL_CONFIG.satoshiMonitorApi}/ws/chat/`
 
   const { connect, on } = useWebSocket({
@@ -29,12 +29,15 @@ export const useChatMonitorMsg = () => {
 
     setSocket(await connect(wssUrl))
 
-    on('message', () => {})
-
     on('event', ({ data }) => {
       console.log(`Keyup: ${useChatStore.getState().inputKeyup}`)
       console.log(`readAnswer: ${useChatStore.getState().readAnswer}`)
       console.log(`waitAnswer: ${useChatStore.getState().waitAnswer}`)
+
+      if (!isLogined) {
+        socket?.close()
+        return
+      }
 
       // 1. Within 20 seconds after the input is in the state of Keyup
       // 2. Within 20 seconds after the latest question answer
@@ -60,6 +63,7 @@ export const useChatMonitorMsg = () => {
     if (isLogined) {
       inithMonitorReq()
     } else {
+      socket?.close()
     }
   }, [isLogined])
 }
