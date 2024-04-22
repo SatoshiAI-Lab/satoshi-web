2f6aa57855bfc7010d978096ab3216af67deef25
l1en <2134045201@qq.com>
2024-04-22 22:15:35 +0800

    chore: modify vercel-deploy

--- vercel-deploy.js
+++ vercel-deploy.js
@@ -4,7 +4,7 @@ console.log('Deploy branch:', process.env.VERCEL_GIT_COMMIT_REF)
 const allows = ['main', 'dev']
 
 if (allows.includes(process.env.VERCEL_GIT_COMMIT_REF)) {
-  console.log('✅ - Build can proceed')
+  console.log('✅ - Build can proceed.')
   return 1
 } else {
   console.log('❌ - Build cancelled, allowed branches:', allows.join(', '))
