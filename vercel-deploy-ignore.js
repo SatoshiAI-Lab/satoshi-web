dfcfadfd66f92f8ca464aa6915bdd34f21853cee
l1en <2134045201@qq.com>
2024-04-22 21:58:51 +0800

    chore: vercel deploy script

--- vercel-deploy-ignore.js
+++ vercel-deploy-ignore.js
@@ -1,8 +1,9 @@
-#!/bin/bash
+console.log(`VERCEL_GIT_COMMIT_REF: ${process.env.VERCEL_GIT_COMMIT_REF}`)
 
-console.log(`VERCEL_GIT_COMMIT_REF: ${$VERCEL_GIT_COMMIT_REF}`)
-
-if ($VERCEL_GIT_COMMIT_REF == 'main' || $VERCEL_GIT_COMMIT_REF == 'dev') {
+if (
+  process.env.VERCEL_GIT_COMMIT_REF === 'main' ||
+  process.env.VERCEL_GIT_COMMIT_REF === 'dev'
+) {
   console.log('✅ - Build can proceed')
   return 1
 } else {
