c063432fcead4de12ceedd97f5e1da7ee82c656b
l1en <2134045201@qq.com>
2024-04-23 16:48:06 +0800

    refactor: optimize theme change

--- src/stores/use-theme-store.ts
+++ src/stores/use-theme-store.ts
@@ -9,9 +9,18 @@ interface Actions {
 }
 
 /**
- * Global theme store
+ * Theme store.
  */
 export const useThemeStore = create<States & Actions>((set) => ({
   isDark: false,
-  setIsDark: (isDark) => set({ isDark }),
+  setIsDark: (isDark) => {
+    set({ isDark })
+
+    if (typeof window === 'undefined') return
+    if (isDark) {
+      window.document.documentElement.classList.add('dark')
+    } else {
+      window.document.documentElement.classList.remove('dark')
+    }
+  },
 }))
