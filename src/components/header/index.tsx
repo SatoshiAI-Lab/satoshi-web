172a8121ab65b2be2f26a862bb025508c308c3c2
l1en <2134045201@qq.com>
2024-04-23 18:01:05 +0800

    refactor: split logo comp

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,26 +6,22 @@ import type { HeaderItem } from './types'
 
 import { MobileHeader } from './mobile'
 import { DesktopHeader } from './desktop'
-import { Routes } from '@/routes'
 import { useResponsive } from '@/hooks/use-responsive'
-import { useThemeStore } from '@/stores/use-theme-store'
 import { SocialLinks } from '../social-links'
 import { LoginButtons } from '../login-buttons'
 import { LangDropdown } from '../lang-dropdown'
+import { Logo } from '../logo'
 
 interface Props extends React.ComponentProps<'div'> {}
+
+const items: HeaderItem[] = []
 
 export const Header = (props: Props) => {
   const { className } = props
   const router = useRouter()
   const { isMobile, isDesktop } = useResponsive()
-  const { isDark } = useThemeStore()
 
-  const items: HeaderItem[] = []
-
-  const onItemClick = (item: HeaderItem) => {
-    router.push(item.route)
-  }
+  const onItemClick = (item: HeaderItem) => router.push(item.route)
 
   return (
     <div className={clsx('sticky top-0 z-50', className)}>
@@ -38,15 +34,7 @@ export const Header = (props: Props) => {
       >
         <div className="flex items-center">
           {isMobile && <MobileHeader items={items} onItemClick={onItemClick} />}
-          <img
-            src={isDark ? 'images/logos/white.png' : 'images/logos/black.png'}
-            alt="logo"
-            className={clsx(
-              'max-w-logo cursor-pointer mt-3 mx-10',
-              'max-sm:mt-4 max-sm:max-w-logo-m max-sm:ml-2'
-            )}
-            onClick={() => router.push(Routes.index)}
-          />
+          <Logo className="mt-3 mx-10 max-sm:mt-4 max-sm:max-w-logo-m max-sm:ml-2" />
           {isDesktop && (
             <DesktopHeader items={items} onItemClick={onItemClick} />
           )}
